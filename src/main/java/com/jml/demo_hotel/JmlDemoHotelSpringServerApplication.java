823f5d8dbc80a2ac715d85687493fc02097e4321
JulianMLaf <julianl@live.com.ar>
2018-04-22 21:41:35 -0300

    Init Spring Boot

---
+++ src/main/java/com/jml/demo_hotel/JmlDemoHotelSpringServerApplication.java
@@ -0,0 +1,12 @@
+package com.jml.demo_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmlDemoHotelSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmlDemoHotelSpringServerApplication.class, args);
+	}
+}
